1600a30f3b1fe459b8af04da410ef49a45b5dd19
yejin-km <124483450+yejin-km@users.noreply.github.com>
2023-11-16 22:55:34 -0500

    added axios and dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [inputValue, setInputValue] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${inputValue}.....`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}
+  }`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleInputValueChange(event) {
