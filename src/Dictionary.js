ba7004a31e3bb1c485eb7b9dfde1965ef6d04ae6
yejin-km <124483450+yejin-km@users.noreply.github.com>
2023-11-17 00:48:09 -0500

    rendered Meaning component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [inputValue, setInputValue] = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log(response.data);
+    setResults(response.data[0]);
   }
+
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}
-  }`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleInputValueChange(event) {
-    setInputValue(event.target.value);
+    setKeyword(event.target.value);
   }
 
   return (
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleInputValueChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
