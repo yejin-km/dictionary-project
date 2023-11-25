725e3f0d5a7ed00263e270939c1c28e0301848a6
yejin-km <124483450+yejin-km@users.noreply.github.com>
2023-11-24 23:16:59 -0500

    added Phonetics component
    added phonetics and fontawesome icon for audio

---
+++ src/Phonetic.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+        <button>
+          <i
+            className="fa-solid fa-volume-high fa-lg"
+            style={{ color: "#307bfd" }}
+          ></i>
+        </button>
+      </a>
+
+      {props.phonetic.text}
+    </div>
+  );
+}
